78f792a3d9abc98d95403187ffeead972fcf3548
o-200 <omgout.200@gmail.com>
2025-10-31 14:12:03 +0700

    add mongo

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { Mongoose } from 'mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    Mongoose.forRoot('mongodb://localhost/nect-me')
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
